602d3a7cc04a4f52dcd3df6daea175f24abe1775
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2024-08-02 14:46:28 +0300

    hw 6 LimitSchedulerThreadExecutor

--- task-3-limit-scheduling-task/src/main/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutor.java
+++ task-3-limit-scheduling-task/src/main/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutor.java
@@ -1,4 +1,60 @@
 package com.github.javarar.limit.scheduler;
 
-public class LimitSchedulerThreadExecutor {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Logger;
+
+public class LimitSchedulerThreadExecutor implements Executor {
+  private final Logger logger = Logger.getLogger(LimitSchedulerThreadExecutor.class.getSimpleName());
+  private Queue<Queue< Runnable>> tasks;
+  private List<Thread> threads = new LinkedList<>();
+  private Integer limit;
+
+
+  private LimitSchedulerThreadExecutor(Integer threadNumber, Integer delay, Integer limit) {
+    this.limit = limit;
+    tasks = new LinkedBlockingQueue<>();
+    for (int i = 0; i < threadNumber; i++) {
+      threads.add(new Thread(() -> {
+        while (!Thread.currentThread().isInterrupted()) {
+          Queue<Runnable> poll = tasks.poll();
+          if(poll != null) {
+            Runnable runnable = poll.poll();
+            if(runnable != null) {
+              if(!poll.isEmpty()){
+                tasks.add(poll);
+              }
+              try {
+                synchronized (runnable) {
+                  runnable.wait(delay);
+                  runnable.run();
+                }
+              } catch (InterruptedException e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        }
+      }));
+    }
+    threads.forEach(Thread::start);
+  }
+
+  public static Executor limitScheduledThreadPoolExecutor(Integer threadNumber, Integer delay, Integer limit) {
+    return new LimitSchedulerThreadExecutor(threadNumber, delay, limit);
+  }
+
+  @Override
+  public void execute(Runnable command) {
+      logger.info("добавления задачи в очередь " + command.hashCode());
+      Queue<Runnable> list = new LinkedList<>();
+      for (int i = 0; i < limit; i++) {
+        list.add(command);
+      }
+      tasks.add(list);
+  }
 }
--- task-3-limit-scheduling-task/src/test/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutorTest.java
+++ task-3-limit-scheduling-task/src/test/java/com/github/javarar/limit/scheduler/LimitSchedulerThreadExecutorTest.java
@@ -1,4 +1,60 @@
 package com.github.javarar.limit.scheduler;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executor;
+import java.util.logging.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LimitSchedulerThreadExecutorTest {
+  private final Logger logger = Logger.getLogger(LimitSchedulerThreadExecutor.class.getSimpleName());
+  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault());
+  private ByteArrayOutputStream logContent;
+
+  @BeforeEach
+  public void setUp() {
+    logContent = new ByteArrayOutputStream();
+    StreamHandler customHandler = new StreamHandler(logContent, new Formatter() {
+      @Override
+      public String format(LogRecord record) {
+        return String.format("%s %s \n", formatter.format(record.getInstant()), record.getMessage());
+      }
+    });
+    LogManager.getLogManager().reset();
+    logger.addHandler(customHandler);
+    logger.setUseParentHandlers(false); // Отключаем обработчики по умолчанию
+  }
+
+
+  @Test
+  public void limitScheduledTaskExecution() throws InterruptedException {
+    Executor executor = LimitSchedulerThreadExecutor.limitScheduledThreadPoolExecutor(1, 1, 17);
+    Runnable runnable = () -> logger.info("задача выполнилась");
+    executor.execute(runnable);
+    executor.execute(runnable);
+    // ждем выполнения задач
+    Thread.sleep(100);
+    // Принудительное завершение обработчика, чтобы все логи были записаны
+    for (Handler handler : logger.getHandlers()) {
+      handler.flush();
+    }
+
+    String logOutput = logContent.toString();
+    String regexForCompletedRunnable = "([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})(.*задача выполнилась.*)";
+    Pattern pattern1 = Pattern.compile(regexForCompletedRunnable);
+    Matcher matcher1 = pattern1.matcher(logOutput);
+    int sum = 0;
+    while (matcher1.find()){
+      sum ++;
+    }
+    assertTrue(sum == 34, "задача была выполнена неверное количество раз");
+    System.out.println(logOutput);
+  }
 }
